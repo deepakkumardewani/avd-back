9384505b2a6aa1b88b701f874d76b95618280cf9
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-03-18 21:19:32 +0530

    ADD: upload helper

---
+++ helpers/upload.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const config = require('../config')
+function uploadFile (req, res, folderPath, cb) {
+  // Configure client for use with Spaces
+  const spacesEndpoint = new AWS.Endpoint(`${config.spacesEndpoint}/${folderPath}`)
+  const s3 = new AWS.S3({
+    endpoint: spacesEndpoint,
+    accessKeyId: '5D43XLAGUY6OKJB7NWNM',
+    secretAccessKey: 'k5jpTC6bCzK7XjWXl52S9iY7cATvKD+BTkgKWHHStfg'
+  })
+
+  const upload = multer({
+    storage: multerS3({
+      s3,
+      bucket: 'avd-bapuji',
+      acl: 'public-read',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      key: function (req, file, cb) {
+        cb(null, file.originalname)
+      }
+    })
+  }).single('upload')
+
+  upload(req, res, function (error) {
+    if (error) {
+      console.error(error)
+      cb(error, null)
+    }
+    cb(null, req.file)
+  })
+}
+
+module.exports = uploadFile
