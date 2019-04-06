2df3e5c8659ea2a1c569d6da0cc44d200c5acef5
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-04-06 22:38:17 +0530

    ADD: cors

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 // const routes = require('./routes')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+
+var whitelist = ['http://localhost:4200', 'https://anandvrindavan.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 
 const photoRoutes = require('./routes/photos')
 const lectureRoutes = require('./routes/lectures')
@@ -16,9 +31,10 @@ const port = process.env.PORT || 3050
 const url = process.env.IP || '0.0.0.0'
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+  // res.setHeader('Access-Control-Allow-Origin', 'https://anandvrindvan.com')
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
-  res.setHeader('Access-Control-Allow-Methods', 'POST, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, DELETE, GET, PUT')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, albumtitle'
